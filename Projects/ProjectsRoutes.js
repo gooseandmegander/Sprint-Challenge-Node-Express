792f490c495cb9f2e00b613eecbe1e13f2223e8b
Megan Williamson <meganawilliamson@gmail.com>
2018-04-06 09:32:34 -0700

    projectRoutes created, get / connected, added helmet and morgan

---
+++ Projects/ProjectsRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const projectsDb = require('../data/helpers/projectModel.js');
+const router = express.Router();
+
+// Endpoints for /api/projects
+
+// get
+router.get('/', (req, res) => {});
+
+// get with id param
+
+// post
+
+// delete
+
+// put
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // express
 const express = require('express');
 const server = express();
+// require middleware
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 // routes
+const projectsRoutes = require('./Projects/ProjectsRoutes.js');
 
 // middleware
-server.use(express.json());
+server.use(morgan('dev')); // logger
+server.use(helmet()); // security
+server.use(express.json()); // parser
+
+server.use('/api/projects', projectsRoutes);
 
 const port = 5000;
 server.listen(port, () => console.log('API running on port 5000...'));
